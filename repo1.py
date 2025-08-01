a2bffb8cbc10b5e33fad23ca5cacad983c230b6a
THEBONELESS06 <anas.hilali2006@gmail.com>
2025-08-01 19:23:46 +0100

    .

---
+++ repo1.py
@@ -0,0 +1,10 @@
+# Notes des trois élèves
+note1_duA = 14
+note2_duY = 17
+note3_duM = 15
+
+# Trouver la meilleure note
+meilleure_note = max(note1_duA, note2_duY, note3_duM)
+
+# Afficher la meilleure note
+print("La meilleure note est :", meilleure_note)
